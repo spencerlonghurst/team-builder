f930ce94a79b91ad52074123ca0cce46f73ef650
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-02-22 16:41:52 -0700

    started project

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import React, { useState } from 'react';
+
+
+
+
+
+
+
+
+
+function App() {
+  //STEP 1. Use state to hold all the values within the form.
+  const [formValues, setFormValues] = useState();
+
+
+
+
+
+  return (
+    <div className="App">
+      <h1>The Facebook Profile App</h1>
+    </div>
+  );
+}
+
+export default App;
